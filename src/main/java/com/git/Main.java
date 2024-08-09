bfac74773860f8385691291c1ad911843b988a28
Judith Huisa <huisaj30@gmail.com>
2024-08-08 20:31:12 -0500

    adding a question

--- src/main/java/com/git/Main.java
+++ src/main/java/com/git/Main.java
@@ -3,5 +3,7 @@ package com.git;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git and TGP in NTT");
+        System.out.println("Hello Git and Teacher in NTT");
+        System.out.println("Hello Git and all in NTT");
     }
 }
